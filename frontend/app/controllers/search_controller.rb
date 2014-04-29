c7524cc270616a0435a8e0b61157e888b7d2ea44
Payten Giles <payten.giles@gmail.com>
2014-04-29 15:29:22 +1000

    Fix staff advanced search when more than two queries

--- frontend/app/controllers/search_controller.rb
+++ frontend/app/controllers/search_controller.rb
@@ -3,26 +3,17 @@ class SearchController < ApplicationController
   set_access_control  "view_repository" => [:do_search, :advanced_search]
 
   def advanced_search
-    @criteria = params_for_backend_search
+    criteria = params_for_backend_search
 
-    terms = (0..2).collect{|i|
-      term = search_term(i)
+    queries = advanced_search_queries.reject{|field| field["value"].blank?}
 
-      if term and term[:op] === "NOT"
-        term[:op] = "AND"
-        term[:negated] = true
-      end
-
-      term
-    }.compact
-
-    if not terms.empty?
-      @criteria["aq"] = JSONModel(:advanced_query).from_hash({"query" => group_queries(terms)}).to_json
-      @criteria['facet[]'] = SearchResultData.BASE_FACETS
+    if not queries.empty?
+      criteria["aq"] = JSONModel(:advanced_query).from_hash({"query" => build_queries(queries)}).to_json
+      criteria['facet[]'] = SearchResultData.BASE_FACETS
     end
 
 
-    @search_data = Search.all(session[:repo_id], @criteria)
+    @search_data = Search.all(session[:repo_id], criteria)
 
     respond_to do |format|
       format.json {
@@ -63,31 +54,65 @@ class SearchController < ApplicationController
 
   private
 
-  def search_term(i)
-    if not params["v#{i}"].blank?
-      { :field => params["f#{i}"], :value => params["v#{i}"], :op => params["op#{i}"] }
-    end
-  end
-
-
-  def group_queries(terms)
-    if terms.length > 1
-      stack = terms.reverse.clone
+  def build_queries(queries)
+    if queries.length > 1
+      stack = queries.reverse.clone
 
       while stack.length > 1
         a = stack.pop
         b = stack.pop
 
         stack.push(JSONModel(:boolean_query).from_hash({
-                                                         :op => b[:op],
-                                                         :subqueries => [JSONModel(:field_query).from_hash(a), JSONModel(:field_query).from_hash(b)]
+                                                         :op => b["op"],
+                                                         :subqueries => [as_subquery(a), as_subquery(b)]
                                                        }))
       end
 
       stack.pop
     else
-      JSONModel(:field_query).from_hash(terms[0])
+      JSONModel(:field_query).from_hash(queries[0])
     end
+  end
+
+
+  def as_subquery(query_data)
+    if query_data.kind_of? JSONModelType
+      query_data
+    else
+      JSONModel(:field_query).from_hash(query_data)
+    end
+  end
+
+
+  helper_method :default_advanced_search_queries
+  def default_advanced_search_queries
+    [{"i" => 0}, {"i" => 1}]
+  end
+
+
+  helper_method :advanced_search_queries
+  def advanced_search_queries
+    return default_advanced_search_queries if !params["advanced"]
+
+    indexes = params.keys.collect{|k| k[/^v(?<index>[\d]+)/, "index"]}.compact.sort{|a,b| a.to_i <=> b.to_i}
+
+    return default_advanced_search_queries if indexes.empty?
+
+    indexes.map {|i|
+      query = {
+        "i" => i.to_i,
+        "op" => params["op#{i}"],
+        "field" => params["f#{i}"],
+        "value" => params["v#{i}"],
+      }
+
+      if query["op"] === "NOT"
+        query["op"] = "AND"
+        query["negated"] = true
+      end
+
+      query
+    }
   end
 
 end
