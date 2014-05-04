2c4186813e47331d23e3d96f8dcdc3c2331ce634
Payten Giles <payten.giles@gmail.com>
2014-05-05 08:56:24 +1000

    Move FrontendEnumSource into common so it can be shared with both the staff and public Rails apps; Fix an exception in the public app where it had no enum_source specified

--- common/db/migrations/030_add_staff_field_query_field_enum.rb
+++ common/db/migrations/030_add_staff_field_query_field_enum.rb
@@ -3,6 +3,7 @@ require_relative 'utils'
 Sequel.migration do
 
   up do
+    $stderr.puts("Adding public and staff interface advanced search field enums")
     create_enum("field_query_field", ["fullrecord", "title", "creators_text", "notes", "subjects_text"], "fullrecord")
     create_enum("staff_field_query_field", ["fullrecord", "title", "creators_text", "notes", "subjects_text"], "fullrecord")
   end
