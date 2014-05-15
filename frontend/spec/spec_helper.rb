a86706beb9ba238658bd96a4ab53cc87c0c29992
Payten Giles <payten.giles@gmail.com>
2014-05-15 18:06:07 +1000

    Fix tricky merge issue whereby my PR renamed the FrontendEnumSource to ClientEnumSource and Mark's PR introduced new frontend tests that check enum's and use the FrontendEnumSource

--- frontend/spec/spec_helper.rb
+++ frontend/spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'jsonmodel'
-require 'frontend_enum_source'
-JSONModel::init(:client_mode => false, :strict_mode => false, :enum_source => FrontendEnumSource.new)
+require 'client_enum_source'
+JSONModel::init(:client_mode => false, :strict_mode => false, :enum_source => ClientEnumSource.new)
 include JSONModel
 
