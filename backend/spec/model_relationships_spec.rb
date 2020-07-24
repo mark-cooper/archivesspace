e879c67d58c663e2ee04bf5e6bdbc13d15001dea
Mark Cooper <mark.c.cooper@outlook.com>
2020-07-23 17:33:22 -0700

    Gems updates

--- backend/spec/model_relationships_spec.rb
+++ backend/spec/model_relationships_spec.rb
@@ -5,55 +5,6 @@ require_relative '../app/model/mixins/relationships'
 
 describe 'Relationships' do
 
-  before(:each) do
-    ## Database setup
-    [:apple, :banana, :cherry].each do |table|
-      $testdb.create_table! table do
-        primary_key :id
-        String :name
-        Integer :lock_version, :default => 0
-
-        Integer :suppressed, :default => 0
-
-        String :created_by
-        String :last_modified_by
-        DateTime :create_time
-        DateTime :system_mtime
-        DateTime :user_mtime
-      end
-    end
-
-    $testdb.create_table! :fruit_salad_rlshp do
-      primary_key :id
-      String :sauce
-      Integer :banana_id
-      Integer :apple_id
-      Integer :suppressed, :null => false, :default => 0
-      Integer :aspace_relationship_position
-      DateTime :system_mtime, :null => false
-      DateTime :user_mtime, :null => false
-      String :created_by
-      String :last_modified_by
-    end
-
-    $testdb.create_table! :friends_rlshp do
-      primary_key :id
-      Integer :banana_id_0
-      Integer :apple_id_0
-      Integer :banana_id_1
-      Integer :apple_id_1
-      Integer :cherry_id
-      Integer :suppressed, :null => false, :default => 0
-
-      Integer :aspace_relationship_position
-      DateTime :system_mtime, :null => false
-      DateTime :user_mtime, :null => false
-      String :created_by
-      String :last_modified_by
-    end
-  end
-
-
   after(:each) do
     Relationships.send(:remove_const, :BananaFruitSalad)
     Relationships.send(:remove_const, :BananaFriends)
--- frontend/app/controllers/archival_objects_controller.rb
+++ frontend/app/controllers/archival_objects_controller.rb
@@ -238,7 +238,7 @@ class ArchivalObjectsController < ApplicationController
           @children.save(:archival_object_id => @parent.id)
         end
 
-        return render :text => I18n.t("rde.messages.success")
+        return render :plain => I18n.t("rde.messages.success")
       rescue JSONModel::ValidationException => e
         @exceptions = @children.children.collect{|c| JSONModel(:archival_object).from_hash(c, false)._exceptions}
         
